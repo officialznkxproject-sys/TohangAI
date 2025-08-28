4b4c4b94fe17d79455ba0b54b5cd7f5964e9253e
officialznkxproject-sys <officialznkxproject@gmail.com>
2025-08-28 17:08:56 +0700

    Create settings.js

---
+++ config/settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+  OWNER_NUMBER: '083131871328',
+  PREFIX: '!',
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost:27017/t-ai',
+  SESSION_PATH: './sessions',
+  // Tambahkan konfigurasi lainnya sesuai kebutuhan
+};
